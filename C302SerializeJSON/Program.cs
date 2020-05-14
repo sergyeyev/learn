9822dd4ffffa8629140c7b39352334a91030c967
sergyeyev <sergyeyev.vova@gmail.com>
2020-05-14 17:25:11 +0300

    + Added Application sample * Source code is buildable

--- C302SerializeJSON/Program.cs
+++ C302SerializeJSON/Program.cs
@@ -1,9 +1,36 @@
 ﻿using System;
+using NStack;
+using Terminal.Gui;
 
 namespace C302SerializeJSON {
     class Program {
-        static void Main(string[] args) {
+        unsafe public static void Main(string[] args) {
+            Application.Init();
+            // Creates the top-level window to show
+            var win = new Window("Serialize JSON") {
+               X = 0,
+               Y = 1,
+               Width = Dim.Fill(),
+               Height = Dim.Fill()
+            };
+            Application.Top.Add(win);
 
+            // Creates a menubar, the item "New" has a help menu.
+            var menu = new MenuBar(new MenuBarItem[] {
+                new MenuBarItem ("_File", new MenuItem [] {
+                    new MenuItem ("_New"  , "Creates new file", null),
+                    new MenuItem ("_Close", "", null),
+                    new MenuItem ("_Quit" , "", () => {Application.Top.Running = false;} )
+                }),
+                new MenuBarItem ("_Edit", new MenuItem [] {
+                    new MenuItem ("_Copy", "", null),
+                    new MenuItem ("C_ut", "", null),
+                    new MenuItem ("_Paste", "", null)
+                })
+            });
+            Application.Top.Add(menu);
+
+            Application.Run();
         }
     }
 }
