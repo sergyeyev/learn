75a890deb526c471956cc2458167959bb3ea2f87
sergyeyev <sergyeyev.vova@gmail.com>
2020-05-15 16:40:33 +0300

    + Added Newtonsoft.JSON library

---
+++ C302SerializeJSON/DataModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace C302SerializeJSON {
+    // 1. класс - элемент универсального справочника
+    public class ReferenceItem {
+        private String FId;
+        private String FName;
+        public ReferenceItem() {
+            FId = "";
+            FName = "";
+        }
+        public String Id {
+            get => FId;
+            set { FId = value; }
+        }
+        public String Name {
+            get => FName;
+            set { FName = value; }
+        }
+    }
+
+    public class CourceItem {
+        public ReferenceItem Currency;
+        public float Buy;
+        public float Sale;
+    }
+
+    public class OrgsItem : ReferenceItem {
+        private String FOldId;
+        private String FAddress;
+        private String FPhone;
+        private String FLink;
+
+        public String OldId {
+            get => FOldId;
+            set { FOldId = value; }
+        }
+        public String Address {
+            get => FAddress;
+            set { FAddress = value; }
+        }
+        public String Phone {
+            get => FPhone;
+            set { FPhone = value; }
+        }
+        public String Link {
+            get => FLink;
+            set { FLink = value; }
+        }
+    }
+
+}
--- C302SerializeJSON/Program.cs
+++ C302SerializeJSON/Program.cs
@@ -4,7 +4,7 @@ using Terminal.Gui;
 
 namespace C302SerializeJSON {
     class Program {
-        unsafe public static void Main(string[] args) {
+        public static void Main(string[] args) {
             Application.Init();
             // Creates the top-level window to show
             var win = new Window("Наше окно тестовго приложения") {
@@ -20,7 +20,10 @@ namespace C302SerializeJSON {
                 new MenuBarItem ("_File", new MenuItem [] {
                     new MenuItem ("_New"  , "Creates new file", null),
                     new MenuItem ("_Close", "", null),
-                    new MenuItem ("_Quit" , "", () => {Application.Top.Running = false;} )
+                    new MenuItem ("Выход" , "", () => {
+                        Application.Top.Running = false;
+                    } 
+                    )
                 }),
                 new MenuBarItem ("_Edit", new MenuItem [] {
                     new MenuItem ("_Copy", "", null),
@@ -30,17 +33,8 @@ namespace C302SerializeJSON {
             });
             Application.Top.Add(menu);
 
-            var LLabel1 = new Label(0, 0, "Элемент класса Label с координатами (0,0) ");
-            var LLabel2 = new Label(1, 1, "Элемент класса Label с координатами (1,1) ");
-            var LLabel3 = new Label(2, 2, "Элемент класса Label с координатами (2,2) ");
-            var LLabel4 = new Label(3, 18, "Элемент класса Label с координатами (3,18) ");
-            var LLabel11 = new Label(-1, 4, "Элемент класса Label с координатами (-1,4) ");
-            var LLabel12 = new Label(-3, 5, "Элемент класса Label с координатами (-3,5) ");
-            var LLabel13 = new Label(150, 6, "Элемент класса Label с координатами (150,6) ");
-
-            win.Add( LLabel1, LLabel2, LLabel3, LLabel4, LLabel11, LLabel12, LLabel13);
-
-            Application.Run();
+ 
+             Application.Run();
         }
     }
 }
