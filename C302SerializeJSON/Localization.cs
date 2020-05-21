fe460856c9709f6b441e04a48cf3832f160192b8
sergyeyev <sergyeyev.vova@gmail.com>
2020-05-21 17:39:25 +0300

    + Added localization object

---
+++ C302SerializeJSON/Localization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace C302SerializeJSON {
+    public class MessagesHolder {
+        public String StrTowns;
+        public String StrRegions;
+        public String StrCurrencies;
+        public String StrOrgTypes;
+        public String StrOrgs;
+        public String StrReferences;
+        public String StrFile;
+        public String StrFileExit;
+        public MessagesHolder() {
+            StrTowns = "Города";
+            StrRegions = "Регионы";
+            StrCurrencies = "Валюты";
+            StrOrgTypes = "Типы организаций";
+            StrOrgs = "Организации";
+            StrFile = "Файл";
+            StrFileExit = "Выход";
+            StrReferences = "Справочники";
+        }
+        public void SaveToFile(String FileName) {
+            String LFileContent = JsonConvert.SerializeObject(this, Formatting.Indented);
+            File.WriteAllText(FileName, LFileContent);
+        }
+        public void LoadFromFile(String FileName) {
+            String LFileContent = File.ReadAllText(FileName);
+            var LObject = JsonConvert.DeserializeObject<dynamic>(LFileContent);
+            foreach(var LObj in LObject) {
+                if(((String)LObj.Name).Equals("StrTowns")) StrTowns = LObj.Value;
+                if(((String)LObj.Name).Equals("StrRegions")) StrRegions = LObj.Value;
+                if(((String)LObj.Name).Equals("StrCurrencies")) StrCurrencies = LObj.Value;
+                if(((String)LObj.Name).Equals("StrOrgTypes")) StrOrgTypes = LObj.Value;
+                if(((String)LObj.Name).Equals("StrOrgs")) StrOrgs = LObj.Value;
+                if(((String)LObj.Name).Equals("StrReferences")) StrReferences = LObj.Value;
+                if(((String)LObj.Name).Equals("StrFile")) StrFile = LObj.Value;
+                if(((String)LObj.Name).Equals("StrFileExit")) StrFileExit = LObj.Value;
+            }
+        }
+}
+
+}
--- C302SerializeJSON/Program.cs
+++ C302SerializeJSON/Program.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 namespace C302SerializeJSON {
     class Program {
         const String CFolderMyApplication = "WorkJSON";
+        const String CFolderLocal = "lang";
         const String CRemoteJSON = "http://resources.finance.ua/ru/public/currency-cash.json";
         const String CLocalJSON = "currency-cash.json";
         // поля для модели данных
@@ -22,6 +23,8 @@ namespace C302SerializeJSON {
         public static Reference Currencies;
         public static Reference OrgTypes;
         public static Organitations Orgs;
+        // служебные поля
+        public static MessagesHolder Msgs;
         // поля для элементов управления
         public static MenuBar MainMenu;
         public static Window WndCities;
@@ -29,6 +32,7 @@ namespace C302SerializeJSON {
         public static Window WndCurrencies;
         public static Window WndOrgTypes;
         public static Window WndOrgs;
+        // 
         // обработчики событий
         public static void miFileExit() {
             Application.Top.Running = false;
@@ -39,8 +43,8 @@ namespace C302SerializeJSON {
                 if(Application.Top.Subviews[i].Id == ATtile) {
                     LResult = (Window)Application.Top.Subviews[i];
                     Application.Top.RemoveAll();
-                    Application.Top.Add(MainMenu);
                     Application.Top.Add(LResult);
+                    Application.Top.Add(MainMenu);
                     LResult.FocusFirst();
                     break;
                 }
@@ -145,35 +149,39 @@ namespace C302SerializeJSON {
             LDefaultPath = Path.Combine(LDefaultPath, CFolderMyApplication);
             // устанавливаем этот путь текущим для приложения
             Directory.SetCurrentDirectory(LDefaultPath);
+            // I.5 создаём попдпапку с локализацие приложения "Мои Документы" \ FolderMyApplication \ CFolderLocal
+            if(!Directory.Exists(CFolderLocal)) { // если нет
+                Directory.CreateDirectory(CFolderLocal); // создаём эту папку
+            }
             return LDefaultPath;
         }
         public static MenuBar AppMainMenu() {
-            MenuItem LmiFileExit = new MenuItem("Выход", "Выход из приложения", () => {
+            MenuItem LmiFileExit = new MenuItem(Msgs.StrFileExit, "", () => {
                 miFileExit();
             });
-            MenuItem LmiRefsCities = new MenuItem("Города", "", () => {
-                WndCities = miRefNew("Города", Cities);
+            MenuItem LmiRefsCities = new MenuItem(Msgs.StrTowns, "", () => {
+                WndCities = miRefNew(Msgs.StrTowns, Cities);
                 Application.Run();
             });
-            MenuItem LmiRefsRegions = new MenuItem("Регионы", "", () => {
-                WndRegions = miRefNew("Регионы", Regions);
+            MenuItem LmiRefsRegions = new MenuItem(Msgs.StrRegions, "", () => {
+                WndRegions = miRefNew(Msgs.StrRegions, Regions);
                 Application.Run();
             });
-            MenuItem LmiRefsCurrencies = new MenuItem("Валюты", "", () => {
-                WndCurrencies = miRefNew("Валюты", Currencies);
+            MenuItem LmiRefsCurrencies = new MenuItem(Msgs.StrCurrencies, "", () => {
+                WndCurrencies = miRefNew(Msgs.StrCurrencies, Currencies);
                 Application.Run();
             });
-            MenuItem LmiRefsOrgTypes = new MenuItem("Типы организаций", "", () => {
-                WndOrgTypes = miRefNew("Типы организаций", OrgTypes);
+            MenuItem LmiRefsOrgTypes = new MenuItem(Msgs.StrOrgTypes, "", () => {
+                WndOrgTypes = miRefNew(Msgs.StrOrgTypes, OrgTypes);
                 Application.Run();
             });
-            MenuItem LmiRefsOrgs = new MenuItem("Организации", "", () => {
-                WndOrgs = miRefNew("Организации", Orgs);
+            MenuItem LmiRefsOrgs = new MenuItem(Msgs.StrOrgs, "", () => {
+                WndOrgs = miRefNew(Msgs.StrOrgs, Orgs);
                 Application.Run();
             });
 
-            MenuBarItem LbmiFile = new MenuBarItem("Файл", new MenuItem[] { LmiFileExit });
-            MenuBarItem LbmiRefs = new MenuBarItem("Справочники", new MenuItem[] {
+            MenuBarItem LbmiFile = new MenuBarItem(Msgs.StrFile, new MenuItem[] { LmiFileExit });
+            MenuBarItem LbmiRefs = new MenuBarItem(Msgs.StrReferences, new MenuItem[] {
                 LmiRefsCities, LmiRefsRegions, LmiRefsCurrencies, LmiRefsOrgTypes, LmiRefsOrgs
             });
             MenuBar LResult = new MenuBar(new MenuBarItem[] { LbmiFile, LbmiRefs });
@@ -190,6 +198,16 @@ namespace C302SerializeJSON {
             // 3. загрузить данные из файла
             String LJSON = File.ReadAllText(Path.Combine(LDefaultPath, CLocalJSON));
             var LCourceObject = JsonConvert.DeserializeObject<dynamic>(LJSON);
+            // 3.1. загрузка локализации
+            Msgs = new MessagesHolder();
+            String[] LLocalFiles = Directory.GetFiles(Path.Combine(LDefaultPath, CFolderLocal));
+            // 3.2. организовать выбор локализации
+            foreach(String LLocalFile  in LLocalFiles) {
+                String LLocalName = Path.GetFileNameWithoutExtension(LLocalFile);
+                if(LLocalName.ToUpper().Equals("BL")) { // заглушка, строку "EN" мы будем брать из настроек
+                    Msgs.LoadFromFile(LLocalFile);
+                }
+            }
             // 4. создаём справочники
             Cities     = new Reference(LCourceObject.cities);
             Regions    = new Reference(LCourceObject.regions);
