448cea64c381596574c2a306a2a4c16914441722
sergyeyev <sergyeyev.vova@gmail.com>
2020-05-28 17:17:47 +0300

    + Added User Interface

--- C303App/DataModel.cs
+++ C303App/DataModel.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.IO;
@@ -12,6 +10,9 @@ namespace C303App {
         public RefItem() {
             Id = 0;
             Name = "";
+        }
+        public override String ToString() {
+            return Id.ToString().PadLeft(8) + " \u2551 " + Name;
         }
     }
     public class LBAuthor: RefItem {
@@ -29,8 +30,12 @@ namespace C303App {
         public LBReader() {
             TicketNo = "";
         }
+        public override String ToString() {
+            return Id.ToString().PadLeft(8) + " \u2551 " + Name.PadRight(60) + " \u2551 " + TicketNo.PadRight(20)+ " \u2551";
+        }
     }
     public class Ref : List<RefItem> {
+        public int TestDataCount = 20;
         public virtual RefItem LoadFromJSONItem(dynamic AJSONObject) {
             RefItem LResult = new RefItem();
 
@@ -38,6 +43,7 @@ namespace C303App {
         }
         public virtual void LoadFromFile(String AFilePath) {
             if(!File.Exists(AFilePath)) {
+                GenTest();
                 return;
             }
             // очистим список
@@ -51,12 +57,57 @@ namespace C303App {
         public virtual void SaveToFile(String AFilePath) {
             File.WriteAllText(AFilePath, JsonConvert.SerializeObject(this));
         }
+        public virtual void GenTest() {
+            for(int i = 0; i < TestDataCount; i++) {
+                Add(new RefItem() { Id = i + 1,  Name = "Item " + ( i + 1 ).ToString()} );
+            }
+        }
     }
-    public class LBAuthors : Ref { 
+    public class LBAuthors : Ref {
+        public override void GenTest() {
+            String[] LCAuthors = new String[] {
+                  "Джейм Фенимор Купер"
+                 ,"Алана По"
+                 ,"Артур Конан-Дойль"
+                 ,"Беклемишев"
+                 ,"Яворский"
+                 ,"Агата Кристи"
+                 ,"Александр Дюма"
+                 ,"Достоевский"
+            };
+            Random Rnd = new Random();
+            for(int i = 0; i < TestDataCount; i++) {
+                int LIndx = Rnd.Next(0, LCAuthors.Length);
+                LBAuthor LItem = new LBAuthor();
+                LItem.Id = i + 1;
+                LItem.Name = LCAuthors[LIndx];
+                Add(LItem);
+            }
+        }
     }
     public class LBBooks : Ref {
     }
     public class LBReaders : Ref {
+        public override void GenTest() {
+            String[] LCReaders = new String[] {
+                  "Иванов И.И."
+                 ,"Иванов Е.А."
+                 ,"Петров С.С."
+                 ,"Петров И.П."
+                 ,"Сидоров С.С."
+                 ,"Сидоров А.В."
+                 ,"Кравченко К.Е."
+                 ,"Коваленко А.А."
+            };
+            Random Rnd = new Random();
+            for(int i = 0; i < TestDataCount; i++) {
+                int LIndx = Rnd.Next(0, LCReaders.Length);
+                LBReader LItem = new LBReader();
+                LItem.Id = i + 1;
+                LItem.Name = LCReaders[LIndx];
+                LItem.TicketNo = "18899500" + i.ToString().PadLeft(3).Replace(" ", "0");
+                Add(LItem);
+            }
+        }
     }
-
 }
--- C303App/Program.cs
+++ C303App/Program.cs
@@ -19,6 +19,59 @@ namespace C303App {
         public static LBAuthors Authors;
         public static LBBooks   Books;
         public static LBReaders Readers;
+        // главное меню
+        public static MenuBar MainMenu;
+        public static MenuBarItem MIFile;
+        public static MenuItem MIFileExit;
+        public static MenuBarItem MIRefs;
+        public static MenuItem MIRefsAuthors;
+        public static MenuItem MIRefsBooks;
+        public static MenuItem MIRefsReaders;
+//        public static MenuBarItem MIView;
+ //       public static MenuBarItem MIViewLanguages;
+        // окна
+        public static Window WndAuthors;
+        public static Window WndBooks;
+        public static Window WndReaders;
+        // методы приложения
+        public static Window miRefNew(String ATtile, Ref ARef) {
+            Window LResult = null;
+            for(int i = 0; i < Application.Top.Subviews.Count; i++) {
+                if(Application.Top.Subviews[i].Id == ATtile) {
+                    LResult = (Window)Application.Top.Subviews[i];
+                    Application.Top.RemoveAll();
+                    Application.Top.Add(LResult);
+                    Application.Top.Add(MainMenu);
+                    LResult.FocusFirst();
+                    break;
+                }
+            }
+            if(null == LResult) {
+                LResult = new Window(ATtile) {
+                    Id = ATtile,
+                    X = 0,
+                    Y = 1, // для меню оставим одну строчку
+                    Width = Dim.Fill(),
+                    Height = Dim.Fill()
+                };
+                ListView LListViewRef = new ListView(
+                    new Rect() {
+                        X = 0,
+                        Y = 0,
+                        Height = 56,
+                        Width = 158
+                    },
+                    ARef);
+                LResult.Add(LListViewRef);
+                Application.Top.Add(LResult);
+            }
+            Application.Run();
+            return LResult;
+        }
+        // обработчики событий
+        public static void miFileExit() {
+            Application.Top.Running = false;
+        }
 
         public static String CheckFoldersSettings() {
             // I. установление путей в приложении
@@ -56,6 +109,24 @@ namespace C303App {
             }
             return LDefaultPath;
         }
+        public static void ObjInit() {
+            // работаем с окнами
+            WndAuthors = null;
+            WndBooks   = null;
+            WndReaders = null;
+            // пункты меню главного меню программы
+            MIFileExit = new MenuItem("Выход", "", () => { miFileExit(); });
+            MIFile = new MenuBarItem("Файл", new MenuItem[] { MIFileExit });
+
+            MIRefsAuthors = new MenuItem("Авторы книг", "", () => { WndAuthors = miRefNew("Авторы"  , Authors); });
+            MIRefsBooks   = new MenuItem("Книги"      , "", () => { WndBooks   = miRefNew("Книги"   , Books  ); });
+            MIRefsReaders = new MenuItem("Читатели"   , "", () => { WndReaders = miRefNew("Читатели", Readers); });
+            MIRefs = new MenuBarItem("Справочники", new MenuItem[] {
+                MIRefsAuthors, MIRefsBooks, MIRefsReaders
+            });
+//            MIView = new MenuBarItem(Msgs.StrView, LbmiLanguages);
+            MainMenu = new MenuBar(new MenuBarItem[] { MIFile, MIRefs});
+        }
         static void Main(string[] args) {
             // проверка рабочего каталога приложения
             AppDefaultPathData = CheckFoldersData();
@@ -67,9 +138,12 @@ namespace C303App {
             Authors.LoadFromFile(Path.Combine(AppDefaultPathData, Consts.FileNameAuthors));
             Books.LoadFromFile  (Path.Combine(AppDefaultPathData, Consts.FileNameBooks  ));
             Readers.LoadFromFile(Path.Combine(AppDefaultPathData, Consts.FileNameReaders));
+
             // создание  интерфейса приложения
-
-
+            ObjInit();
+            Application.Init();
+            Application.Top.Add(MainMenu);
+            Application.Run();
 
             // сохраняем данные при выходе из приложения
             if(null != Authors) { Authors.SaveToFile(Path.Combine(AppDefaultPathData, Consts.FileNameAuthors)); }
