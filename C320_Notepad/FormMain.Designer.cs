921b4b5bc9a5d6059c4d4e5b59d235dd29e62fff
sergyeyev <sergyeyev.vova@gmail.com>
2020-06-09 17:47:47 +0300

    + Added notepad Application

---
+++ C320_Notepad/FormMain.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace C320_Notepad {
+    partial class FormMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && ( components != null )) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.TextBoxMain = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TextBoxMain
+            // 
+            this.TextBoxMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TextBoxMain.BackColor = System.Drawing.SystemColors.Window;
+            this.TextBoxMain.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxMain.Location = new System.Drawing.Point(0, 0);
+            this.TextBoxMain.Multiline = true;
+            this.TextBoxMain.Name = "TextBoxMain";
+            this.TextBoxMain.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.TextBoxMain.Size = new System.Drawing.Size(801, 451);
+            this.TextBoxMain.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextBoxMain);
+            this.Name = "FormMain";
+            this.Text = "Блокнот";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxMain;
+    }
+}
+
---
+++ C320_Notepad/FormMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C320_Notepad {
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+        }
+    }
+}
